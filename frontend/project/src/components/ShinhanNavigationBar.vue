15984ea857a48f51caa6f16e82dfe45bc28d440a
jaehyuk <jaehyuk.pyon@gmail.com>
2023-02-20 01:16:41 +0900

    feat: make header, footer and /mileage page

---
+++ frontend/project/src/components/ShinhanNavigationBar.vue
@@ -0,0 +1,94 @@
+<template>
+  <nav>
+    <ol class="shinhan-header" ref="header">
+      <li class="shinhan-header-link">
+        <a href="#" :class="currentPath === '/news' ? 'active' : 'inactive'"
+          >뉴스<br />커뮤니티</a
+        >
+      </li>
+      <li class="shinhan-header-link">
+        <a href="#" :class="currentPath === '/mileage' ? 'active' : 'inactive'"
+          >친환경<br />마일리지</a
+        >
+      </li>
+      <li class="shinhan-header-link">
+        <a href="#" :class="currentPath === '/account' ? 'active' : 'inactive'"
+          >친환경<br />계좌</a
+        >
+      </li>
+      <li class="shinhan-header-link">
+        <a href="#" :class="currentPath === '/mission' ? 'active' : 'inactive'"
+          >친환경<br />미션</a
+        >
+      </li>
+      <li class="shinhan-header-link">
+        <a href="#" :class="currentPath === '/stock' ? 'active' : 'inactive'"
+          >친환경<br />주식</a
+        >
+      </li>
+      <li class="shinhan-header-link">
+        <img
+          :src="require('../assets/shinhannavigationbar/bell-new.svg')"
+          alt=""
+        />
+      </li>
+    </ol>
+  </nav>
+</template>
+
+<script>
+export default {
+  props: ["currentPath"],
+};
+</script>
+
+<style scoped>
+.active {
+  font-weight: bold!important;
+  font-size: 1rem!important;
+}
+
+.inactive {
+  font-weight: lighter!important;
+  color: grey!important;
+  font-size: 1rem!important;
+}
+.shinhan-header {
+  list-style-type: none;
+  overflow: hidden;
+  background-color: white;
+}
+
+.shinhan-header > li {
+  float: left;
+  width: 80px;
+  height: 75px;
+  display: table;
+  position: relative;
+}
+
+.shinhan-header li a {
+  width: 100%;
+  height: 100%;
+  font-size: 14px;
+  color: black;
+  text-decoration: none;
+
+  display: table-cell;
+  vertical-align: middle;
+  text-align: center;
+}
+
+.shinhan-header li img {
+  position: absolute;
+  margin: auto;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+}
+
+.shinhan-header li a:hover {
+  font-weight: bold;
+}
+</style>
---
+++ frontend/project/src/components/StatusBar.vue
@@ -0,0 +1,17 @@
+<template>
+  <header>
+    <img src="../assets/phonestatus/status-bar.svg" alt="">
+  </header>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+.status-bar {
+  height: 52.5px;
+}
+</style>
