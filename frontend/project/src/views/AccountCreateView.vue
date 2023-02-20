5b33561ec4af98eba1c24fe2d3a207ed38d3a954
JhyunJ1 <jhyunj1005@gmail.com>
2023-02-21 08:31:58 +0900

    feat: modify account create view

--- frontend/project/src/views/AccountCreateView.vue
+++ frontend/project/src/views/AccountCreateView.vue
@@ -1,10 +1,36 @@
-<template>
-    <div class="temple">
+<link rel="preconnect" href="https://fonts.googleapis.com"></link>
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin></link>
+<link href="https://fonts.googleapis.com/css2?family=Merriweather:wght@900&family=Noto+Sans+KR:wght@100;400&display=swap" rel="stylesheet"></link>
 
+<template>
+    <greenbar></greenbar>
+    <div class="temple">
+        <p style="font-family: 'Noto Sans KR', sans-serif">친환경 계좌</p>
     </div>
 </template>
+
 <style>
-#temple {
-    background-color: grey;
+
+.temple {
+    background-color: #5BDEB2;
+    height: 480px;
 }
 </style>
+
+<script>
+    import GreenBar from "../components/GreenBar.vue";
+    export default {
+    emits: ["barType"],
+    data() {
+        return {
+        }
+    },
+    methods: {
+
+    },
+    beforeCreate() {
+        this.$emit("barType", "green");
+        this.$emit("sendCurrentPath", this.$route.path);
+    },
+    };
+</script>
