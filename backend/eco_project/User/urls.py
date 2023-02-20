a09cb6a4aeeb9f2a2e4f52cab2bcf00a5fcdeef2
HYEON-CODE <gus7264@gmail.com>
2023-02-20 14:02:00 +0900

    feat: update mileage

--- backend/eco_project/User/urls.py
+++ backend/eco_project/User/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from .views import UserList, UserDetail, createUser
+from rest_framework.authtoken.views import obtain_auth_token
 
 urlpatterns = [
     path('', UserList.as_view(), name='user-list'),
     path('<int:pk>/', UserDetail.as_view(), name='user-detail'),
     path('create/', createUser, name='user-create'),
+    path('login/', obtain_auth_token, name='api_token_auth'),
 ]
--- backend/eco_project/mileage/views.py
+++ backend/eco_project/mileage/views.py
@@ -2,6 +2,13 @@ from rest_framework import generics
 from .models import Mileage
 from .serializers import MileageSerializer
 
-class MileageCreateView(generics.CreateAPIView):
+# POST 기능 (마일리지 획득)
+class MileageList(generics.ListCreateAPIView):
+    queryset = Mileage.objects.all()
+    serializer_class = MileageSerializer    
+
+# GEt, UPDATE, PUT기능 
+class MileageDetail(generics.RetrieveUpdateAPIView):
     queryset = Mileage.objects.all()
     serializer_class = MileageSerializer
+
