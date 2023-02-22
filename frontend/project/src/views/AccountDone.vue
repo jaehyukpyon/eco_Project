d36bd452f3d38f62be2d40b74f814ccff7670e3c
Agayeon <agy125@naver.com>
2023-02-22 13:17:02 +0900

    feat: AccountDone.vue fin

--- frontend/project/src/views/AccountDone.vue
+++ frontend/project/src/views/AccountDone.vue
@@ -1,18 +1,19 @@
 <template>
-    <div class="temple">
-        <p class="title">친환경 계좌 <br>
-            <span class="small">완료되었습니다!</span> 
+    <meta charset="UTF-8">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Do+Hyeon&display=swap" rel="stylesheet">
+    <div>
+        <p>
+        <span class="title" style="margin-top:150px; text-align: center; position: relative; width: 100%;">친환경 계좌</span><br />
+        <span class="sub" style="margin-top: 0px; text-align: center; position: relative; width:100%;">개설이 완료되었습니다!</span>
         </p>
     </div>
-<span class="sub" style="margin-top: 0px; text-align: center; position: relative; width:100%;">친환경 계좌 개설 완료!</span>
-<div v-if="type === 'account'"></div>
-        <img
-          src="../assets/account/account.png"
-          width="480"
-          height="160"
-          alt=""
-        />
-<div class="done">
+    <div style="text-align: center; margin-top: 65px;">
+        <img src="../assets/account/account.png" alt="">
+      </div>
+  
+<div class="done" style="margin-top:100px;">
     <button id="confrim" v-on:click="btn_confrim">확인</button>
 </div>
 </template>
@@ -26,17 +27,24 @@ export default {
     },
     methods: {
         btn_confrim: function(event) {
-            alert('개설 완료!')
+            alert('친환경 계좌 개설이 완료되셨습니다!')
         }
     }
 }
 </script>
 
 <style scoped>
+.title {
+  display: inline-block;
+  font-weight: normal;
+  font-size: 45px;
+  position: center;
+  color: #25BF8B;
+}
 .sub {
   display: inline-block;
   font-weight: light;
-  font-size: 15px;
+  font-size: 35px;
   position: center;
 }
 .done {
@@ -45,11 +53,16 @@ export default {
     font-size: 20px;
     color: white;
     position: center;
-    background-color: #25bf8b;
 }
-.temple {
-    background-color: #5BDEB2;
-    height: 480px;
-}
+button {
+    background-color: #25BF8B;
+    color: white;
+    padding: 18px 225.25px;
+    margin: 8px 0;
+    border: none;
+    cursor: pointer;
+    width: 100%;
+    font-size: 1.2rem;
+  }
 
 </style>
