17269bd606e19c381baad5c48a7971764afeecd1
HYEON-CODE <gus7264@gmail.com>
2023-02-22 13:16:23 +0900

    feat: Crawling the Latest News

--- backend/eco_project/news/views.py
+++ backend/eco_project/news/views.py
@@ -3,7 +3,9 @@ from rest_framework.response import Response
 from rest_framework import status
 import requests
 from bs4 import BeautifulSoup
+from selenium import webdriver
 
+# RE100 사이트를 GET
 class InnerTextView(APIView):
     def get(self, request, format=None):
         inner_text_list = []
@@ -17,3 +19,31 @@ class InnerTextView(APIView):
                 span_texts = [span.get_text(strip=True) for span in span_elements]
                 inner_text_list.append(span_texts)
         return Response(inner_text_list, status=status.HTTP_200_OK)
+
+
+# 최신 뉴스를 GET
+class NewsView(APIView):
+    def get(self, request):
+        url = "https://www.sedaily.com/Cube/CubeCollect/249"
+
+        driver = webdriver.Chrome()  # Change this to the path of your chromedriver executable
+        driver.get(url)
+        soup = BeautifulSoup(driver.page_source, 'html.parser')
+        driver.quit()
+
+        collect_list = soup.select('#collectList > ul > li')[:10]
+
+        news_result=[]
+        for item in collect_list:
+            result = []
+            news_href = item.select_one('a')['href']
+            thumb_img = item.select_one('.thumb span img')['src']
+            text_title = item.select_one('.text_area h3').get_text(strip=True)
+            text_sub = item.select_one('.text_sub').get_text(strip=True)
+            result.append(news_href)
+            result.append(thumb_img)
+            result.append(text_title)
+            result.append(text_sub)
+            news_result.append(result)
+
+        return Response(news_result, status=status.HTTP_200_OK)
